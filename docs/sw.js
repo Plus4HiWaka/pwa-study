ed93dd36b64c93983c0ac10f93e29432babccfed
Hiroshi Wakasugi <hi.wakasugi.plus4@gmail.com>
2017-08-10 14:15:49 +0900

    Update sw.js

---
+++ docs/sw.js
@@ -0,0 +1,17 @@
+var CACHE_NAME = 'pwa-study-cache-v1';
+var urlsToCache = [
+  '/',
+  '/img/ckw.gif',
+  '/img/ckwman.jpg'
+];
+
+self.addEventListener('install', function(event) {
+  // インストール処理
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
