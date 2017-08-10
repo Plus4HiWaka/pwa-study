cc507871d4e32b868880d1c3e01f8c888572171d
Hiroshi Wakasugi <hi.wakasugi.plus4@gmail.com>
2017-08-10 14:53:09 +0900

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -2,10 +2,22 @@ var CACHE_NAME = 'pwa-study-cache-v1';
 var urlsToCache = [
   '.',
   'img/favicon.ico',
-  'img/ckw.gif',
-  'img/ckwman.jpg',
-  'img/ckwpan.jpg',
-  'img/ckwtank.png'
+  'css/bootstrap.css',
+  'css/bootstrap.css.map',
+  'css/bootstrap.min.css',
+  'css/bootstrap.min.css.map',
+  'css/bootstrap-theme.css',
+  'css/bootstrap-theme.css.map',
+  'css/bootstrap-theme.min.css',
+  'css/bootstrap-theme.min.css.map',
+  'fonts/glyphicons-halflings-regular.eot',
+  'fonts/glyphicons-halflings-regular.svg',
+  'fonts/glyphicons-halflings-regular.ttf',
+  'fonts/glyphicons-halflings-regular.woff',
+  'fonts/glyphicons-halflings-regular.woff2',
+  'js/bootstrap.js',
+  'js/bootstrap.min.js',
+  'js/npm.js'
 ];
 
 self.addEventListener('install', function(event) {
